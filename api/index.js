17fcaa5a707d31ed24d05fdf609693432c639919
RathodAnilT <132031578+RathodAnilT@users.noreply.github.com>
2025-06-27 09:07:29 +0530

    Update all dependencies to latest versions and fix Express 5 compatibility

--- api/index.js
+++ api/index.js
@@ -40,10 +40,6 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
